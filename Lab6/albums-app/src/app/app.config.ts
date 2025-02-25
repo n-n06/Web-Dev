80d41cdac1fdb5bb02396b8df94a70a5318543b1
nurs <nursultanzhantuar@gmail.com>
2025-02-26 01:49:30 +0500

    Add: lab6: all required components - implement later)

--- Lab6/albums-app/src/app/app.config.ts
+++ Lab6/albums-app/src/app/app.config.ts
@@ -2,11 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideImageKitLoader } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
-    provideImageKitLoader('https://ik.imagekit.io/nspectree/', )
+    provideRouter(routes)
   ]
 };
---
+++ Lab6/albums-app/src/app/pages/albums-list/albums-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-albums-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './albums-list.component.html',
+  styleUrl: './albums-list.component.css'
+})
+export class AlbumsListComponent {
+
+}
