29006c45ac08399d906dd25f9227c35e19e8471c
n-n06 <nursultanzhantuar@gmail.com>
2025-03-12 01:17:34 +0500

    Add: Lab6 - task1 - start informatics

--- Lab6/albums-app/src/app/pages/about/about.component.ts
+++ Lab6/albums-app/src/app/pages/about/about.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { ImageUploadComponent } from "../../components/image-upload/image-upload.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [ImageUploadComponent],
+  imports: [],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
