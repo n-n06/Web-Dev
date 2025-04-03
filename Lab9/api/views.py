9b67f97a5a8581d25dcba5ca75e2ab89710af13d
n-n06 <nursultanzhantuar@gmail.com>
2025-04-03 11:48:21 +0500

    Add - final tweaks, fix by company search

--- Lab9/api/views.py
+++ Lab9/api/views.py
@@ -18,6 +18,13 @@ class VacancyViewSet(viewsets.ModelViewSet):
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
 
+    def get_queryset(self):
+        company_id = self.kwargs.get('company_pk')
+        if company_id:
+            return Vacancy.objects.filter(company_id=company_id).order_by('id')
+        return Vacancy.objects.all().order_by('id')
+
+
     @action(detail=False, methods=['get'], url_path='top_ten')
     def top_ten(self, request):
         vacancies = Vacancy.objects.order_by('-salary')[:10]
